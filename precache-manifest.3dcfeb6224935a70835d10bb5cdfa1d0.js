2bac5b76fb379c08da551a0ed6d772b9a408fc4d
Sammael001 <sylviaisworking@gmail.com>
2020-07-27 13:59:08 -0500

    Updates

---
+++ precache-manifest.3dcfeb6224935a70835d10bb5cdfa1d0.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc255920379dbfc17fc1376d7e746bec",
+    "url": "/Sammael001/my-react-games/index.html"
+  },
+  {
+    "revision": "3f29b2edf5d982db5d51",
+    "url": "/Sammael001/my-react-games/static/css/main.c885eacb.chunk.css"
+  },
+  {
+    "revision": "b6cf50011c2a94cce401",
+    "url": "/Sammael001/my-react-games/static/js/2.deb7dd1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Sammael001/my-react-games/static/js/2.deb7dd1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f29b2edf5d982db5d51",
+    "url": "/Sammael001/my-react-games/static/js/main.3b6c49a4.chunk.js"
+  },
+  {
+    "revision": "647f8faaba25857ed09a",
+    "url": "/Sammael001/my-react-games/static/js/runtime-main.dfc5a681.js"
+  },
+  {
+    "revision": "7059fd29828a9abfff8cabcbbe73835e",
+    "url": "/Sammael001/my-react-games/static/media/ankh-fade.7059fd29.jpg"
+  },
+  {
+    "revision": "4295ab9e929755d3ea619a206c948111",
+    "url": "/Sammael001/my-react-games/static/media/ankh.4295ab9e.jpg"
+  },
+  {
+    "revision": "112d9515cb229bb7f1f86a731324487b",
+    "url": "/Sammael001/my-react-games/static/media/bats-fade.112d9515.jpg"
+  },
+  {
+    "revision": "fdecda3d9b27505f679c91557b6602a6",
+    "url": "/Sammael001/my-react-games/static/media/bats.fdecda3d.jpg"
+  },
+  {
+    "revision": "09bfc535e73bb6c1cc2af3d7a0f5fee6",
+    "url": "/Sammael001/my-react-games/static/media/blood-fade.09bfc535.jpg"
+  },
+  {
+    "revision": "a95dc3dbd13ba7865b921baa1c043991",
+    "url": "/Sammael001/my-react-games/static/media/blood.a95dc3db.jpg"
+  },
+  {
+    "revision": "fa2e2c6271f5dc824fed375e7567d319",
+    "url": "/Sammael001/my-react-games/static/media/candles2-fade.fa2e2c62.jpg"
+  },
+  {
+    "revision": "a08f09cc139f6fa3874a97e95dca38ff",
+    "url": "/Sammael001/my-react-games/static/media/candles2.a08f09cc.jpg"
+  },
+  {
+    "revision": "d38b5ad2cdf3a1bbf1448df813646a2f",
+    "url": "/Sammael001/my-react-games/static/media/castle-fade.d38b5ad2.jpg"
+  },
+  {
+    "revision": "b80311a86ab23061c0f23b01b6ec8a1d",
+    "url": "/Sammael001/my-react-games/static/media/castle.b80311a8.jpg"
+  },
+  {
+    "revision": "ccfdb1c35d4db32f8db2f3f8cb5f88e4",
+    "url": "/Sammael001/my-react-games/static/media/castle.ccfdb1c3.jpg"
+  },
+  {
+    "revision": "37726f10f67b53892ca496407c63c131",
+    "url": "/Sammael001/my-react-games/static/media/cup-fade.37726f10.jpg"
+  },
+  {
+    "revision": "4f55b3fa53f94c676e7bb6e40602515f",
+    "url": "/Sammael001/my-react-games/static/media/cup.4f55b3fa.jpg"
+  },
+  {
+    "revision": "225e397d159606cc6cf88e3836633128",
+    "url": "/Sammael001/my-react-games/static/media/fangs2-fade.225e397d.jpg"
+  },
+  {
+    "revision": "9ce2e7469b5e6ac3c07479153384a71b",
+    "url": "/Sammael001/my-react-games/static/media/fangs2.9ce2e746.jpg"
+  },
+  {
+    "revision": "e3b94f13b18c0131cb91dcaa2d210e5e",
+    "url": "/Sammael001/my-react-games/static/media/lake.e3b94f13.jpg"
+  },
+  {
+    "revision": "6d4df5a681cd271e3511110bb6b5c68d",
+    "url": "/Sammael001/my-react-games/static/media/mist.6d4df5a6.jpg"
+  },
+  {
+    "revision": "41bffd1aa150ea11ac802db816f2004f",
+    "url": "/Sammael001/my-react-games/static/media/moon.41bffd1a.jpg"
+  }
+]);
